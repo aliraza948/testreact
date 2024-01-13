3c94fce45e42178f9ad8238e3d26863457d81758
aliraza948 <aliraza948533@gmail.com>
2024-01-13 23:39:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import {MsgChecker,AboutUs,ContactUs,} from './components/NavBar.js'
 import {
   Routes,
+  HashRouter,
   Route,
-  Link,
   BrowserRouter,
 } from "react-router-dom";
 
@@ -12,42 +12,28 @@ function App() {
     <>
   
 
-       {/*<BrowserRouter>
+  <HashRouter>
         <nav className="py-5 bg-zinc-900 flex justify-between">
         <div className="flex"><input type="text" className='ml-4 rounded-l-sm p-1'/><button type='button' className='text-black bg-white rounded-r-sm shadow-inner shadow-black px-4'>Press it</button> </div>
         
         <div className='flex justify-end'>
         <ul className="flex space-x-10 text-slate-100 pr-4">
-            
-        <Link to="">Home</Link>
-        <Link to="">Contact</Link>
-        <Link to="">Contact Us</Link></ul>
+        
+        <a href="/">Home</a>
+        <a href="/contact"> contact</a>
+        <a href="/about"> about</a>
+        
+        </ul>
+        
            
         </div>
        </nav>
        <Routes>
-        <Route path="/">
-          <MsgChecker heading="good"/>
-        </Route>
-        <Route path="/About">
-          <AboutUs/>
-        </Route>
-        <Route path="/ContactUs">
-          <ContactUs/>
-        </Route>
-       </Routes>
-  </BrowserRouter> */}
- <b>fd</b>
-  <BrowserRouter>
-      <Routes>
-        <Route path="/" >
-          <Route index element={<MsgChecker heading="Ali"/>} />
-          <Route path="contact" element={<ContactUs/>} />
-          <Route path="about" element={<AboutUs/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  
+        <Route path="/" element={<MsgChecker/>}/>
+        <Route path="/contact" element={<ContactUs/>}/>
+        <Route path="/about" element={<AboutUs/>}/>
+        </Routes>
+       </HashRouter>
    </>
   );
 }
